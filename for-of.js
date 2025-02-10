29b31ae7aa58ab13a0f0b2f882062eb3a0105fae
HasanCodeX <dev.hasan10@gmail.com>
2025-02-11 02:24:48 +0600

    first commit

---
+++ for-of.js
@@ -0,0 +1,18 @@
+const numbers = [1,2,3,4,5];
+
+console.log(numbers);
+
+// for (const num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ['Apple', 'Banana', 'Orange'];
+
+for (const fruit of fruits){
+    console.log(fruit);
+}
+
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log('I love ' + fruit);
+}
---
+++ recap.js
@@ -0,0 +1,14 @@
+// canst : valau do not chamnge
+// let : value can change
+// var : value can change
+
+const amount = 1200;
+
+if(amount > 1000){
+    console.log('Amount is greater than 1000');
+}else{
+    console.log('Amount is less than 1000');
+}
+
+//array
+const numbers = [1,2,3,4,5];
