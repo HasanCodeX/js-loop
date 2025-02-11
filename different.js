4462f5b7b13b97430f7f5136d6fcc71d3a9af3fa
HasanCodeX <dev.hasan10@gmail.com>
2025-02-12 00:15:28 +0600

    19.6

---
+++ different.js
@@ -0,0 +1,29 @@
+// odd numbers
+//  for (let i = 0; i < 20; i++) {
+//     if (i%2!==0) {
+//         console.log(i);
+//     }}
+//  for (let i = 1; i < 30; i+=2) {
+    
+//         console.log(i);
+//     }   
+
+    //give me the last numbers between 1 to 30 dis visible by 5
+
+    // for (let i = 1; i < 30; i++) {
+    //     if (i%5===0) {
+    //         console.log(i);
+    //     }
+    // }
+
+    // for (let i = 1; i < 30; i++) {
+    //     if (i%3===0) {
+    //         console.log(i);
+    //     }
+    // }
+
+    for (let i = 1; i < 30; i++) {
+        if (i%3===0 && i%5===0) {
+            console.log(i);
+        }
+    }
