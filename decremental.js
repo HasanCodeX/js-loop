1df7babd23318fdd339402414fef97a362d129d5
HasanCodeX <dev.hasan10@gmail.com>
2025-02-11 19:53:59 +0600

    explore while loop

---
+++ decremental.js
@@ -0,0 +1,12 @@
+// for(let i= 0; i <= 5; i++){
+//     console.log(i);
+// }
+
+// for (let i = 20; i >= 11; i--){
+//     console.log(i);
+// }
+
+let n =10;
+while (n >= 1){
+    console.log(n);
+    n--;}
